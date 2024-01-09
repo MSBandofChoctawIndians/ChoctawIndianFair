706dc8b606263509320893775f44dc6ceed7f493
Sonny Mattera <smattera@hotmail.com>
2024-01-09 14:41:01 -0600

    Update dependencies and add env.d.ts

---
+++ env.d.ts
@@ -0,0 +1,11 @@
+import { KVNamespace } from "@cloudflare/workers-types";
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      [key: string]: string | undefined;
+      MY_KY: KVNamespace;
+    }
+  }
+}
+
+export {};
