4e63615dfda43dc30b9d6fa903a0454bbd1717d8
Sonny Mattera <smattera@hotmail.com>
2024-05-14 13:09:32 -0500

    refactor: Update route paths for ChoctawIndianFair in main.tsx and Events.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -34,9 +34,9 @@ const router = createBrowserRouter(
   )
 );
 
-ReactDOM.render(
+// @ts-ignore
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
