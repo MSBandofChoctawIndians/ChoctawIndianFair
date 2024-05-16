58e948628a1717ba5c67cb025838da3a46b75249
Sonny Mattera <smattera@hotmail.com>
2024-05-16 13:04:30 -0500

    refactor: Update image URLs and route paths for ChoctawIndianFair in tailwind.config.js, main.tsx, Events.tsx, and NavBar.tsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     extend: {
       backgroundImage: {
         "red-weave":
-          "linear-gradient(135deg, rgba(128,0,0,0.65), transparent, rgba(128,0,0,0.65)), url('./img/weave02.svg')",
+          "linear-gradient(135deg, rgba(128,0,0,0.65), transparent, rgba(128,0,0,0.65)), url('/ChoctawIndianFair/img/weave02.svg')",
         rope: "linear-gradient(transparent, transparent, rgba(255,0,0,0.25)), url('/ChoctawIndianFair/img/rope.svg')",
         "sponsor-background":
           "url('/ChoctawIndianFair/img/lattice.svg'), linear-gradient(to bottom, rgba(255,0,0,0.15), transparent, transparent, rgba(255,0,0,0.15))",
